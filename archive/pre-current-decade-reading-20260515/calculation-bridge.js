b1d1bfe6c9d6954f5d9a88ff400133105d01c923
cathy <cathy@cathydeMac-mini.local>
2026-05-15 00:20:59 +0900

    Deploy current decade fortune reading

---
+++ archive/pre-current-decade-reading-20260515/calculation-bridge.js
@@ -0,0 +1,6 @@
+import { calculateShichusuimei, LOCATIONS } from "./calculation-lab.js?v=user-home-20260515-luck-1";
+
+window.HOSHI_CALC = {
+  calculateShichusuimei,
+  LOCATIONS,
+};
--- calculation-bridge.js
+++ calculation-bridge.js
@@ -1,4 +1,4 @@
-import { calculateShichusuimei, LOCATIONS } from "./calculation-lab.js?v=user-home-20260515-luck-1";
+import { calculateShichusuimei, LOCATIONS } from "./calculation-lab.js?v=user-home-20260515-decade-1";
 
 window.HOSHI_CALC = {
   calculateShichusuimei,

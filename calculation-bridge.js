f92ab29f816fd1b182660e2118761fa460d2e237
cathy <cathy@cathydeMac-mini.local>
2026-05-16 19:54:51 +0900

    Deploy user page backend sync

--- calculation-bridge.js
+++ calculation-bridge.js
@@ -1,4 +1,4 @@
-import { calculateShichusuimei, LOCATIONS } from "./calculation-lab.js?v=user-home-20260515-decade-1";
+import { calculateShichusuimei, LOCATIONS } from "./calculation-lab.js?v=20260516-user-sync-1";
 
 window.HOSHI_CALC = {
   calculateShichusuimei,
